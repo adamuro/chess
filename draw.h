5ebdaefe8ca8ae87e1148b1bc448d8f060bc74aa
Adam Turowski <58310442+adamuro@users.noreply.github.com>
2020-02-03 17:08:28 +0100

    Delete draw.h

--- draw.h
+++
@@ -1,9 +0,0 @@
-#ifndef DRAW_H
-#define DRAW_H
-
-#include "common.h"
-
-void drawBoard (RenderWindow *Window, RectangleShape *Board);
-void drawPieces (RenderWindow *Window, Texture *piecesTextures, int *piecesOnBoard);
-
-#endif
